72436b34422fff4a8304afbb1b3e321bdf17e465
Killarun <ququsko@gmail.com>
2021-01-31 17:33:48 +0300

    newCommit

--- js/main.js
+++ js/main.js
@@ -2,12 +2,12 @@ function getRandomInteger(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   if (max <= min){
-    return 'Ошибка: Числа равны, диапазона нет.';
+    return 'Ошибка: Минимальное число больше или равно максимальному.';
   }
   else{
     if (min >= 0 && max >= 0)
     {
-    return Math.floor(Math.random() * (max - min + 1)) + min;}
+      return Math.floor(Math.random() * (max - min + 1)) + min;}
   }
   return 'Ошибка: Числа дожны быть больше 0.';
 }
@@ -16,14 +16,14 @@ getRandomInteger(1, 1);
 
 
 function getRandomLocation (min, max, one) {
-  if (min > max) {
-    return 'Ошибка: Минимальное число больше максимального';
+  if (min >= max) {
+    return 'Ошибка: Минимальное число больше или равно максимальному';
 
   } else {
     if (min >= 0 && max >= 0)
     {
-    return (Math.random() * (max - min) + min).toFixed(one);}
+      return (Math.random() * (max - min) + min).toFixed(one);}
   }
   return 'Ошибка: Числа дожны быть больше 0.';
 }
-getRandomLocation(2,4,1);
+getRandomLocation(1,1,1);
