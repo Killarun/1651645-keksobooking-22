3e359e30b2fc59aa74be6636843318df934fd427
Killarun <ququsko@gmail.com>
2021-01-31 21:59:38 +0300

    Module2_Fix2

--- js/main.js
+++ js/main.js
@@ -2,16 +2,16 @@ function getRandomInteger(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   if (max <= min || min < 0) {
-    return false;
+    return null;
   }
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-console.log(getRandomInteger(1, 1));
+getRandomInteger(1, 1);
 
 
 function getRandomNumber (min, max, range) {
   if (max <= min || min < 0)  {
-    return false;
+    return null;
   }
   return Number((Math.random() * (max - min) + min).toFixed(range));
 }
